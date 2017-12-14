90ec0af31d247af7c964c0e5c4bb22b3a8e6dfb3
dov11 <ovo.dan@gmail.com>
2017-12-14 12:25:01 +0100

    add some comments

--- routes/students.js
+++ routes/students.js
@@ -15,7 +15,7 @@ router.get('/batches/:id1/:id2', (req, res, next) => {
   })
   .catch((error) => next(error))
 })
-.patch('/batches/:id',
+  .patch('/batches/:id',
   // authenticate,
   (req, res, next) => {
   const id = req.params.id
@@ -30,30 +30,35 @@ router.get('/batches/:id1/:id2', (req, res, next) => {
 
     .catch((error) => next(error))
 })
-.patch('/batches/:id1/:id2',
+  .patch('/batches/:id1/:id2',
   // authenticate,
   (req, res, next) => {
-  const id1 = req.params.id1
-  const id2 = req.params.id2
-  let evaluation = req.body
-  evaluationDateNew = new Date(evaluation.evaluationDate).toDateString()
-  Batch.findById(id1)
-  .then((batch) => {
-    if(!batch) {return next()}
-    //clusterfuck below :'( needs refactoring
-    const updatedStudentsWithEvals = batch.students.map(student=>{
-      if (student._id==id2) {
-        let evaluationDates=student.performanceCodes.map(code=>code.evaluationDate.toDateString())
-        let updatedCodes = evaluationDates.includes(evaluationDateNew) ?
-        student.performanceCodes.map(code=>{
-          if (code.evaluationDate.toDateString()===evaluationDateNew) {
-            return evaluation
-          }
-          return code
-        })
-         :
-        [evaluation].concat(student.performanceCodes)
-        return {
+    const id1 = req.params.id1
+    const id2 = req.params.id2
+    let evaluation = req.body
+    evaluationDateNew = new Date(evaluation.evaluationDate).toDateString()
+    Batch.findById(id1)
+      .then((batch) => {
+      if(!batch) {return next()}
+      //clusterfuck below :'( needs refactoring
+      const updatedStudentsWithEvals = batch.students.map(student=>{
+        if (student._id==id2) {
+
+          let evaluationDates=student.performanceCodes.map(code=>code.evaluationDate.toDateString())
+
+          //overwrighting or adding evaluations
+          let updatedCodes = evaluationDates.includes(evaluationDateNew) ?
+          student.performanceCodes.map(code=>{
+            if (code.evaluationDate.toDateString()===evaluationDateNew) {
+              return evaluation
+            }
+            return code
+          })
+           :
+          [evaluation].concat(student.performanceCodes)
+          //
+
+          return {
           _id: student._id,
           firstName: student.firstName,
           lastName: student.lastName,
@@ -61,17 +66,18 @@ router.get('/batches/:id1/:id2', (req, res, next) => {
           performanceCodes: updatedCodes}
       }
       return student
-    })
+      })
+
     Batch.findByIdAndUpdate(id1, { students: updatedStudentsWithEvals }, { new: true })
-    .then((batch) => res.json(batch))
-  })
+      .then((batch) => res.json(batch))
+    })
 
 
     .catch((error) => {
       console.log(error)
       next(error)
     })
-})
+  })
 .delete('/batches/:idB/:idS',
 // authenticate,
 (req, res, next) => {
